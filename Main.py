0dd4342185dfacd169afef6409568e1e5d28d2a3
Lee Seungun <seungund@naver.com>
2023-07-19 13:15:41 +0900

    Bot logging feature

--- Main.py
+++ Main.py
@@ -1,14 +1,29 @@
+#Please read it 
+#커밋 풀 시 .env파일 추가 --> 토큰 값
+
 # 모듈 가져오기
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
-from discord.ext import commands
 
 # 봇 기본 설정
 
 load_dotenv()
-
 token = os.getenv("TOKEN") #토큰 값 가져오기
 
-bot = commands.Bot(command_prefix='/') #봇 명령어가 / 으로 시작함
-bot.run(token)
+intents = discord.Intents.default()  # 기본 인텐트 설정
+intents.members = True  # 서버 멤버 목록을 읽을 수 있는 인텐트 설정
+
+client = discord.Client(intents=intents)
+
+#client = commands.Bot(command_prefix='/') #봇 명령어가 / 으로 시작함
+#///////////////////////////////////////////////////////////////////#
+
+@client.event
+async def on_ready():
+    print('im online')
+
+
+#///////////////////////////////////////////////////////////////////#
+client.run(token)
