aa1bb7891201762a64fc717cd417d154db962542
seungund <seungund@naver.com>
2023-07-24 21:00:29 +0900

    DEV) add image, hello command

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ token = os.getenv("TOKEN") #토큰 값 가져오기
 intents = discord.Intents.all()  # 기본 인텐트 설정
 intents.members = True  # 서버 멤버 목록을 읽을 수 있는 인텐트 설정
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 #///////////////////////////////////////////////////////////////////#
 
@@ -27,6 +27,19 @@ async def on_ready():
 async def on_message(message):
     if message.content == "안녕":
         await message.channel.send("안녕하쇼")
+    await bot.process_commands(message)
+        
+@bot.command() # !Hello 입력 시 Hello 출력
+async def Hello(ctx):
+    await ctx.send("Hello")
+
+@bot.command() # !image + 사진파일 입력 시 이미지 링크 반환
+async def image(ctx):
+    print(ctx.message.attachments[0].url)
+    await ctx.send("확인")
+    return(ctx.message.attachments[0].url)
+
+
 
 #///////////////////////////////////////////////////////////////////#
 bot.run(token)
