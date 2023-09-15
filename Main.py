ef5560b7761f1c8e2ca0772d57e0a47dbb215fec
Lee Seungun <seungund@naver.com>
2023-09-15 13:52:52 +0900

    DEV) readme , command 수정

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 # 봇 기본 설정
 
 load_dotenv()
-token = os.getenv("TOKEN") #토큰 값 가져오기
+token = os.getenv("TOKEN") # .env 파일에서 토큰 값 가져오기
 
 intents = discord.Intents.all()  # 기본 인텐트 설정
 intents.members = True  # 서버 멤버 목록을 읽을 수 있는 인텐트 설정
@@ -29,8 +29,8 @@ async def on_message(message):
         await message.channel.send("안녕하쇼")
     await bot.process_commands(message)
         
-@bot.command() # !Hello 입력 시 Hello 출력
-async def Hello(ctx):
+@bot.command() # !hello 입력 시 hello 출력
+async def hello(ctx):
     await ctx.send("Hello")
 
 @bot.command() # !image + 사진파일 입력 시 이미지 링크 반환
--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 감정분석 알고리즘을 통해 대화만으로 감정파악 (1학년 챗봇과 감정분석 알고리즘을 융합) 독거노인 같은 취약계층의 위험도 측정
 
-### 모델 설명
+## 모델 설명
+
+### 모델 1
+
+### 모델 2
 
 학습 데이터 : https://www.kaggle.com/datasets/ananthu017/emotion-detection-fer 이미지 사용
 
 Google Teachable Machine을 사용하여 학습
 
+
+### 기간
 ~2023/12/22
